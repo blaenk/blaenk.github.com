6ce3bf1a9e9dee0097281301c247463e86ec2ac3
Jorge Israel Peña <jorgepblank@gmail.com>
2013-01-23 23:12:42 -0800

    async load a few js

--- plugins/jekyll_asset_pipeline.rb
+++ plugins/jekyll_asset_pipeline.rb
@@ -39,4 +39,24 @@ module JekyllAssetPipeline
       end
     end
   end
+
+  class JavaScriptTagTemplate < JekyllAssetPipeline::Template
+    def self.filetype
+      '.js'
+    end
+
+    def html
+      "<script async='true' src='/#{@path}/#{@filename}' type='text/javascript'></script>\n"
+    end
+  end
+
+  class CssTagTemplate < JekyllAssetPipeline::Template
+    def self.filetype
+      '.css'
+    end
+
+    def html
+      "<link href='/#{@path}/#{@filename}' rel='stylesheet' type='text/css' media='screen' />\n"
+    end
+  end
 end
